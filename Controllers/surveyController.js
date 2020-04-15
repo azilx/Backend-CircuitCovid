dc87f11abe7aaa89792da4a3c4a4b70067ed89ee
LaouiniSofiene <sofiene.laouini@esprit.tn>
2020-04-15 21:02:43 +0100

    Revert "Revert "Survery Crud""
    This reverts commit 86b4cd07f4e3670e1c97f1cf416f8a279800d954.

---
+++ Controllers/surveyController.js
@@ -0,0 +1,19 @@
+var survey = require('../Models/survey');
+
+exports.add = function(data){
+    survey.create(data);
+}
+exports.getAll = function(){
+    return survey.find();
+}
+exports.getById = function(id) {
+    return survey.findById(id);
+}
+exports.remove = function(id){
+    return survey.findById(id);
+}
+exports.update = function(id,obj){
+    survey.findByIdAndUpdate({_id : id},obj,function(err,doc){
+        
+    });
+}
---
+++ Models/survey.js
@@ -0,0 +1,20 @@
+const config = require('../config');
+var mongoose = require('mongoose');
+const doctor = require('./doctor');
+const patient = require('./patient');
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.db, {useNewUrlParser: true,useUnifiedTopology: true});
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var SurveySchema = new mongoose.Schema({
+    name : String ,
+    questions : [String],
+    answers : [String],
+    doctor : {type:doctor.schema , ref:'Doctor'},
+    patient : {type:patient.schema , ref:'Patient'}
+});
+module.exports =  mongoose.model('Survey',SurveySchema);
