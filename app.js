84ae4496833b97c40f79327c5b21cdfff303b08b
LaouiniSofiene <sofiene.laouini@esprit.tn>
2020-04-15 21:01:56 +0100

    Survery Crud

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var doctorRouter = require('./routes/doctorRoute');
 var patientRouter = require('./routes/patientRoute');
+var surveyRouter = require('./routes/surveyRoute');
 var app = express();
 
 app.use(cors());//Default cors allowall we have to change heders
@@ -42,6 +43,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/doctor', doctorRouter);
 app.use('/patient', patientRouter);
+app.use('/survey', surveyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
