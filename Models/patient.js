586e7eec5ab9c195b690e0c5b53ef71c2a778936
LaouiniSofiene <sofiene.laouini@esprit.tn>
2020-04-25 15:15:23 +0100

    Patient Structure Updated

--- Models/patient.js
+++ Models/patient.js
@@ -10,12 +10,95 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var PatientSchema = new mongoose.Schema({
-    name : String ,
-    cin: String ,
-    age: String ,
-    gender : String ,
+    name: {type: String ,required: true},
+    age: {type: Number ,required: true},
+    gender : {type: String , enum: ["Male", "Female"],required: true},
+    email: String,
+    cin: {type: String ,required: true},
+    phone: {type: String ,required: true},
+    address: {type: String, required: true},
     risky : Boolean ,
     pendingSurvey : Boolean,
-    alert : Boolean
+    alert : Boolean,
+    reasonForHospitalisation: [{
+        symptoms: [{
+            dyspnea: Boolean,
+            chestPain: Boolean,
+            fever: Boolean,
+            asthenia: Boolean,
+            anosmia: Boolean,
+            ageusia: Boolean,
+            cough: Boolean,
+            expectoration: Boolean,
+            confusion: Boolean,
+            diarrhea: Boolean,
+            aeg: Boolean,
+            nausea: Boolean,
+            vomit: Boolean
+        }],
+        distress: [{
+            respiratoryDistress: Boolean,
+            neurologicalDistress: Boolean,
+            hemodynamicInstability: Boolean,
+            metabolicEmergency: Boolean
+        }]
+    }],
+    symptomsStartDate: {type: Date,required: true},
+    firstSymptoms: String,
+    background: [{
+        chronicPathologies: [{
+            hta: Boolean,
+            diabetes: Boolean,
+            acFa: Boolean,
+            heartFailure: Boolean,
+            CoronaryArtery: Boolean,
+            bpco: Boolean,
+            asthma: Boolean,
+            ischemicStroke: Boolean,
+            hemorrhagicStroke: Boolean,
+            hemodialysis: Boolean,
+            immunosuppression: Boolean,
+            generalIllness: Boolean,
+            activeCancer: Boolean,
+            RenalFailure: Boolean
+        }],
+        usualTreatment: {type: String, required: true},
+        recentTreatment: [{
+            ains: Boolean,
+            corticotherapy: Boolean,
+            immunosuppressant: Boolean,
+            chemotherapy: Boolean
+        }]
+    }],
+    typeOfContact: [{
+        originFromAnEndemicArea: Boolean,
+        contactWithAPositiveCovid: Boolean,
+        ContactWithASuspectedCase: Boolean,
+        respectForIsolation: Boolean,
+        familyMembers: Number
+    }],
+    consultation: [{
+        fr: {type: Number, required: true, min: 0, max: 50},
+        spo2: {type: Number, required: true, min: 30, max: 100},
+        fio2: {type: Number,  min: 21, max: 100},
+        pas: {type: Number, required: true, min: 0, max: 30},
+        pad: {type: Number, required: true, min: 0, max: 20},
+        fc: {type: Number, required: true, min: 0, max: 300},
+        gcs: {type: Number, required: true, min: 3, max: 15},
+        gad: {type: Number, min: 0.1, max: 6},
+        spo2: {type: Number, required: true, min: 30, max: 100},
+        sous: [{
+            nad: Boolean,
+            dob: Boolean,
+            adr: Boolean,
+            seduced: Boolean,
+            signsOfStruggles: Boolean,
+            coldEnds: Boolean,
+            marbrure: Boolean,
+            fine: Number,
+            igs2: Number,
+            sofa: Number
+        }]
+    }]
 });
 module.exports =  mongoose.model('Patient',PatientSchema);
