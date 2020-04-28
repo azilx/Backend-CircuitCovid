7450d3866530d7c4f03ad7828f350c705688436c
naustrad <ali.garouachi@esprit.tn>
2020-04-28 22:44:45 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Merci pour vos efforts a tous', { title: 'Circuit Covid' });
+  res.send('Merci pour vos efforts a tous , circuit covid backend is well deployed and running');
 });
 
 module.exports = router;
