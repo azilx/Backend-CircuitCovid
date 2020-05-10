e1ec4e5662f1e6d25564d560c0876e37f2854ede
azilx <63718977+azilx@users.noreply.github.com>
2020-05-10 22:25:57 +0200

    no Token, patientRoute.js

--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var patientController = require('../Controllers/patientController');
-const verifyToken = require("../middleware/jwt");
 
-router.get('/all', verifyToken, function(req, res, next) {
+
+router.get('/all', function(req, res, next) {
     patientController.getAll().exec().then(data =>{
         res.json(data);
     })
 });
-router.post('/getByDoctor', verifyToken, function(req, res, next) {
+router.post('/getByDoctor', function(req, res, next) {
   patientController.getByDoctor(req.body.doctor._id).exec().then(data =>{
       res.json(data);
   })
 });
-router.post('/getByID', verifyToken, function(req, res, next) {
+router.post('/getByID', function(req, res, next) {
   patientController.getById(req.body._id).exec().then(data =>{
       res.json(data);
   })
 });
-router.post('/add', verifyToken, function(req, res, next) {
+router.post('/add', function(req, res, next) {
     console.log(req.body);
     patientController.add(req.body);
     res.status(201).send({
@@ -26,15 +26,7 @@ router.post('/add', verifyToken, function(req, res, next) {
       error: '',
   });
 });
-router.post('/addMany', verifyToken, function(req, res, next) {
-  console.log(req.body);
-  patientController.addMany(req.body.patients);
-  res.status(201).send({
-    status: true,
-    error: '',
-});
-});
-router.put('/update', verifyToken, function (req, res, next) {
+router.put('/update', function (req, res, next) {
   console.log(req.body);
   patientController.update(req.body._id,req.body);
   res.status(200).send({
@@ -42,7 +34,7 @@ router.put('/update', verifyToken, function (req, res, next) {
     error: '',
 });
 })
-router.delete('/delete', verifyToken, function (req, res, next) {
+router.delete('/delete', function (req, res, next) {
   console.log(req.body);
   patientController.remove(req.body._id).exec().then(data=>{
     if(data)
@@ -50,7 +42,7 @@ router.delete('/delete', verifyToken, function (req, res, next) {
     res.json(data);
   })
 })
-router.post('/setPendingOff', verifyToken, function (req, res, next) {
+router.post('/setPendingOff', function (req, res, next) {
   patientController.setPendingSurveyOff(req,res);
 });
 module.exports = router;
