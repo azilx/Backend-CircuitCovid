bd6c2abd123fe896ba02a88b02a87d2aa2bfbc6e
naustrad <ali.garouachi@esprit.tn>
2020-05-12 17:46:27 +0100

    Update scheduleTime.js

--- scheduleTime.js
+++ scheduleTime.js
@@ -1,16 +1,4 @@
 module.exports = {
     Surveyschedule : [
-        { 
-            name : 'firstwave',
-            date : '1995-12-17T13:37:00',
-            patient : 'all',
-            alertAfter : 3 
-        },
-        { 
-            name : 'Secondwave',
-            date : '1995-12-17T13:38:00',
-            patient : 'risky',
-            alertAfter : 3 
-        }
     ]
 }
